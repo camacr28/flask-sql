c446f6c2bfbd0f5a7397c08ae32a92e69975fe19
camacr28 <camacr28@gmail.com>
2023-10-03 20:18:57 +0200

    Mostramos movimientos en tabla template

--- balance/views.py
+++ balance/views.py
@@ -1,8 +1,13 @@
 from flask import render_template
 
 from . import app
+from .models import DBManager
 
 
 @app.route('/')
 def home():
-    return render_template('inicio.html')
+    db = DBManager('balance/data/balance.db')
+    sql = 'SELECT id, fecha, concepto, tipo, cantidad FROM movimientos'
+    movimientos = db.consultaSQL(sql)
+
+    return render_template('inicio.html', movs=movimientos)
