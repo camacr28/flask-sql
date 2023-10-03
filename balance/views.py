a731634010afd3c6c57c401a204fd3d64c1b88dd
camacr28 <camacr28@gmail.com>
2023-10-03 20:41:46 +0200

    Preparamos template borrado

--- balance/views.py
+++ balance/views.py
@@ -11,3 +11,13 @@ def home():
     movimientos = db.consultaSQL(sql)
 
     return render_template('inicio.html', movs=movimientos)
+
+# - Función borrar
+# - Operar con la BD
+# - Poner un botón de borrado en cada movimiento
+# - Plantilla con el resultado
+
+
+@app.route('/borrar')
+def eliminar():
+    return render_template('borrado.html', resultado=False)
